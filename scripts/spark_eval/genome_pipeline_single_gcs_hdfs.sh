acb5e58064ed4e2c3544ae2ce122104e387e7fb0
Tom White <tom@cloudera.com>
2017-07-24 12:59:48 +0100

    Use WGS-G94982-NA12878.bam with NC_007605 contig removed since it is not in the reference.

--- scripts/spark_eval/genome_pipeline_single_gcs_hdfs.sh
+++ scripts/spark_eval/genome_pipeline_single_gcs_hdfs.sh
@@ -4,4 +4,4 @@
 
 . utils.sh
 
-time_gatk "ReadsPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs://${GCS_CLUSTER}-m:8020/user/$USER/q4_spark_eval/out/WGS-G94982-NA12878.vcf -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs://${GCS_CLUSTER}-m:8020/user/$USER/q4_spark_eval/dbsnp_138.b37.vcf -XL NC_007605 -pairHMM AVX_LOGLESS_CACHING -maxReadsPerAlignmentStart 10" 10 8 42g 4g
+time_gatk "ReadsPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878-no-NC_007605.bam -O hdfs://${GCS_CLUSTER}-m:8020/user/$USER/q4_spark_eval/out/WGS-G94982-NA12878.vcf -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs://${GCS_CLUSTER}-m:8020/user/$USER/q4_spark_eval/dbsnp_138.b37.vcf -pairHMM AVX_LOGLESS_CACHING -maxReadsPerAlignmentStart 10" 10 8 42g 4g
